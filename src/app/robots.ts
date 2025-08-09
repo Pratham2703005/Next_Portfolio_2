fb9063fa6352a0d2fc73629df9578186e78ccad3
Pratham Israni <pk2732004@gmail.com>
2025-08-09 15:03:42 +0530

    robot

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'https://pratham-potfolio.vercel.app';
+const baseUrl = (process.env.NEXT_PUBLIC_BASE_URL || 'https://pratham-potfolio.vercel.app').replace(/\/$/, '');
 
   return {
     rules: {
